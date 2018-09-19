07ef21e71b2552e2f8aaf274dc8e36a490f1ce2c
Twonny98 <42622799+Twonny98@users.noreply.github.com>
2018-09-19 18:45:24 -0400

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Transform startMarker;
+    public Transform endMarker;
+    public bool repeatable = false; 
+
+    public float speed = 1.0f;
+    private float startTime;
+    private float journeyLegnth;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+        //Keep a nite of the time movement started
+        startTime = Time.time;
+
+        //Claculate Journery Length
+        journeyLegnth = Vector3.Distance(startMarker.position, endMarker.position);
+
+        while(repeatable)
+        {
+            yield return RepeatLerp(startMarker.position, endMarker.position, 3.0f);
+            yield return RepeatLerp(endMarker.position, startMarker.position, 3.0f);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!repeatable)
+        {
+            //Distance ,oved = time * speed
+            float distCovered = (Time.time - startTime) * speed;
+
+            //Fraction of Journey completed = current distance / total distance
+            float fracJourney = distCovered / journeyLegnth;
+
+            //Set our position as a fraction of the distance between the markers
+            transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+        }
+        //transform.position = Vector3.Lerp(endMarker.position, startMarker.position, fracJourney);
+
+    }
+
+    public IEnumerator RepeatLerp(Vector3 a, Vector3 b, float time)
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            this.transform.position = Vector3.Lerp(a, b, i);
+            yield return null;
+        }
+    }
+}
--- PlayerController.cs
+++ PlayerController.cs
@@ -7,16 +7,21 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     public Text countText;
+    public Text scoreText;
     public Text winText;
+    public PlayerController Player;
 
     private Rigidbody rb;
     private int count;
+    private int score;
+    private bool stagechange = false;
 
 	// Use this for initialization
 	void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        score = 0;
         SetCountText();
         winText.text = "";
 	}
@@ -25,10 +30,16 @@ public class PlayerController : MonoBehaviour {
     {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
+        float colorR = Mathf.Abs(Player.transform.position.x / 10);
+        float colorG = Mathf.Abs(Player.transform.position.y / 10);
+        float colorB = Mathf.Abs(Player.transform.position.z / 10);
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Color colornow = new Vector4(colorR, colorG, colorB, 0.0f);
 
-        rb.AddForce (movement * speed);
+        Player.GetComponent<Renderer>().material.color = colornow;
+
+        rb.AddForce(movement * speed);
     }
 
     void OnTriggerEnter(Collider other)
@@ -37,6 +48,13 @@ public class PlayerController : MonoBehaviour {
         {
             other.gameObject.SetActive(false);
             count++;
+            score++;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            score--;
             SetCountText();
         }
     }
@@ -44,13 +62,21 @@ public class PlayerController : MonoBehaviour {
     void SetCountText ()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12)
-            winText.text = "You Win!";
+        scoreText.text = "Score: " + score.ToString();
+        if (count >= 22)
+            winText.text = "You Finished with a Score of: " + score.ToString();
         return;
     }
 
     void Update()
     {
+        if (stagechange != true)
+            if (count >= 12)
+            {
+                transform.position = new Vector3(18f, .5f, 5f);
+                stagechange = true;
+            }
+
         if (Input.GetKey("escape"))
         {
             Application.Quit();
