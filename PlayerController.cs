15be70ce3c09b21c9baf1eb1d99b038739d52267
Twonny98 <42622799+Twonny98@users.noreply.github.com>
2018-10-07 17:16:55 -0400

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,57 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
     public float speed;
     public Text countText;
     public Text winText;
 
-    private Rigidbody rb;
+    private Rigidbody2D rb2d;
     private int count;
 
-	// Use this for initialization
-	void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb2d = GetComponent<Rigidbody2D>();
         count = 0;
-        SetCountText();
         winText.text = "";
-	}
-
-    private void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce (movement * speed);
+        SetCountText();
     }
 
-    void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (other.gameObject.CompareTag("Pick Up"))
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVeritcal = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVeritcal);
+        rb2d.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
             count++;
             SetCountText();
         }
+        
     }
 
-    void SetCountText ()
+    void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
         if (count >= 12)
-            winText.text = "You Win!";
-        return;
-    }
-
-    void Update()
-    {
-        if (Input.GetKey("escape"))
         {
-            Application.Quit();
+            winText.text = "You Win!";
         }
     }
+
 }
