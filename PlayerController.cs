47e0d517acb2b23d0e34defa80858d053d172dd4
Twonny98 <42622799+Twonny98@users.noreply.github.com>
2018-10-07 17:18:15 -0400

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,59 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed;
-    public Text countText;
-    public Text winText;
+    private Rigidbody2D rb2d;
 
-    private Rigidbody rb;
-    private int count;
+    public float speed;
+    public float jumpForce;
 
 	// Use this for initialization
-	void Start()
+	void Start ()
     {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText();
-        winText.text = "";
+        rb2d = GetComponent<Rigidbody2D>();		
 	}
-
-    private void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce (movement * speed);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Pick Up"))
-        {
-            other.gameObject.SetActive(false);
-            count++;
-            SetCountText();
-        }
-    }
-
-    void SetCountText ()
-    {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 12)
-            winText.text = "You Win!";
-        return;
-    }
-
-    void Update()
+	
+	// Update is called once per frame
+	void Update ()
     {
         if (Input.GetKey("escape"))
-        {
             Application.Quit();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
         }
     }
 }
