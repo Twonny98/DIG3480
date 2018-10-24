9bbae6d5b6598e4e60bc617ae0ea3ba9e76ce864
Twonny98 <42622799+Twonny98@users.noreply.github.com>
2018-10-23 23:06:36 -0400

    Add files via upload

--- CameraController.cs
+++ CameraController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public GameObject player;
+    public Transform player;
 
     private Vector3 offset;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
-        transform.position = player.transform.position + offset;
-	}
+	void LateUpdate ()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
 }
--- PlayerController.cs
+++ PlayerController.cs
@@ -1,59 +1,131 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+    private Rigidbody2D rb2d;
+    private bool facingRight = true;
+    private Animator anim;
+
     public float speed;
-    public Text countText;
-    public Text winText;
+    public float jumpforce;
 
-    private Rigidbody rb;
-    private int count;
+    //ground check
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
 
-	// Use this for initialization
-	void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText();
-        winText.text = "";
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+
+    //audio stuff
+    public AudioClip jumpSound;
+    public AudioClip coinSound;
+    private AudioSource source1;
+    private AudioSource source2;
+   
+    
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        
 	}
 
-    private void FixedUpdate()
+    void Awake()
     {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
 
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+       source1 = GetComponent<AudioSource>();
+       source2 = GetComponent<AudioSource>(); 
 
-        rb.AddForce (movement * speed);
     }
 
-    void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (other.gameObject.CompareTag("Pick Up"))
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        Debug.Log(isOnGround);
+
+        //for running animation
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+
+
+        //stuff I added to flip my character
+        if(facingRight == false && moveHorizontal > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveHorizontal < 0)
+        {
+            Flip();
+        }
+
+	}
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickups"))
         {
             other.gameObject.SetActive(false);
-            count++;
-            SetCountText();
+            source2.PlayOneShot(coinSound, 1f);
         }
     }
 
-    void SetCountText ()
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 12)
-            winText.text = "You Win!";
-        return;
-    }
-
-    void Update()
-    {
-        if (Input.GetKey("escape"))
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
-            Application.Quit();
+   
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+               rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+               rb2d.velocity = Vector2.up * jumpforce;
+
+
+                // Audio stuff
+
+                source1.PlayOneShot(jumpSound, 1f);
+                
+            }
         }
     }
 }
